a0f00c4ed003ef75738ed97135c75d20137db4bf
ProfitBricks <supports@profitbricks.com>
2012-06-28 18:06:27 +0300

    Updated version to 1.2.2

--- src/pbapi.py
+++ src/pbapi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #
-# v1.2.1 Copyright 2012 ProfitBricks GmbH
+# v1.2.2 Copyright 2012 ProfitBricks GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
