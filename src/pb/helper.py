cefe9b4a0e49efb459354612a4a011d9658f13d7
Ingo Fischer <ingo.fischer@profitbricks.com>
2012-09-21 11:42:23 +0200

    Replace tabs with spaces (as suggested in PEP-8)

--- src/pb/helper.py
+++ src/pb/helper.py
@@ -1,23 +1,23 @@
 import re
 
 class Helper:
-	
-	@staticmethod
-	def camelCaseToDash(s):
-		s1 = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', s)
-		return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s1).lower()
-	
-	@staticmethod
-	def printOperations(operations):
-		print "Available operations and mandatory arguments:"
-		for op in sorted(operations):
-			print ":",\
-				Helper.camelCaseToDash(re.sub("@", "", op)),\
-				("(-" + " -".join(operations[op]["args"]) + ")") if len(operations[op]["args"]) > 0 else "",\
-				"(internal)" if re.search("@", op) else ""
-	
-	@staticmethod
-	def printOperationsSimple(operations):
-		for op in sorted(operations):
-			print Helper.camelCaseToDash(re.sub("@", "", op))
+     
+     @staticmethod
+     def camelCaseToDash(s):
+          s1 = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', s)
+          return re.sub('([a-z0-9])([A-Z])', r'\1-\2', s1).lower()
+     
+     @staticmethod
+     def printOperations(operations):
+          print "Available operations and mandatory arguments:"
+          for op in sorted(operations):
+               print ":",\
+                    Helper.camelCaseToDash(re.sub("@", "", op)),\
+                    ("(-" + " -".join(operations[op]["args"]) + ")") if len(operations[op]["args"]) > 0 else "",\
+                    "(internal)" if re.search("@", op) else ""
+     
+     @staticmethod
+     def printOperationsSimple(operations):
+          for op in sorted(operations):
+               print Helper.camelCaseToDash(re.sub("@", "", op))
 
